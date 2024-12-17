9f3581db5fde3c066c660de16a4ec0cd8fd6732b
howyoujini <howyoujini@gmail.com>
2024-12-17 16:16:50 +0900

    feat: implement useDebounceCallback hook

--- src/shared/hooks/useDebounce.tsx
+++ src/shared/hooks/useDebounce.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState(value);
@@ -12,4 +12,25 @@ export function useDebounce<T>(value: T, delay: number): T {
   }, [value, delay]);
 
   return debouncedValue;
+}
+
+export function useDebounceCallback<T extends (...args: any[]) => void>(
+  callback: T,
+  delay: number
+) {
+  const timer = useRef<NodeJS.Timeout | null>(null);
+
+  const debouncedCallback = useCallback(
+    (...args: Parameters<T>) => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+      timer.current = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    },
+    [callback, delay]
+  );
+
+  return debouncedCallback;
 }
