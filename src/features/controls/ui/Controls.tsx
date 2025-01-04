be4f396b05d577ea403e295bcecbb674ef6a85d7
howyoujini <howyoujini@gmail.com>
2025-01-05 00:27:20 +0900

    feat: rearrange buttons inside the controls

--- src/features/controls/ui/Controls.tsx
+++ src/features/controls/ui/Controls.tsx
@@ -5,7 +5,6 @@ import { BoardController } from "./BoardController";
 import { DeleteAllCacheButton } from "./DeleteAllCacheButton";
 import EditingToggle from "./EditingToggle";
 import { NameController } from "./NameController";
-import { ZoomController } from "./ZoomController";
 
 export function Controls({
   mode,
@@ -20,22 +19,18 @@ export function Controls({
 }) {
   return (
     <>
-      <div className="fixed left-0 top-0 z-50 m-4 flex flex-row gap-1 rounded-2xl border border-neutral-200 bg-white px-2 py-2 text-xs drop-shadow-2xl dark:border-zinc-700 dark:bg-black">
+      <div className="fixed left-0 top-0 z-50 m-4 flex flex-row gap-1 rounded-2xl border border-neutral-200 bg-white py-2 pl-3 pr-4 text-xs drop-shadow-2xl dark:border-zinc-700 dark:bg-black">
+        <BoardController />
+        <NameController />
+      </div>
+
+      <div className="fixed right-0 top-0 z-50 m-4 flex flex-row gap-2 rounded-2xl border border-neutral-200 bg-white px-3 py-2 text-xs drop-shadow-2xl dark:border-zinc-700 dark:bg-black">
         <BackgroundPatternToggle
           pattern={pattern}
           onPatternChange={onPatternChange}
         />
         <EditingToggle mode={mode} onModeChange={onModeChange} />
-      </div>
-
-      <div className="fixed left-14 top-0 z-50 m-4 flex flex-row gap-1 rounded-2xl border border-neutral-200 bg-white py-2 pl-3 pr-4 text-xs drop-shadow-2xl dark:border-zinc-700 dark:bg-black">
-        <BoardController />
-        <NameController />
-      </div>
-
-      <div className="fixed right-0 top-0 z-50 m-4 flex flex-row gap-2 rounded-2xl border border-neutral-200 bg-white px-2 py-2 text-xs drop-shadow-2xl dark:border-zinc-700 dark:bg-black">
-        <div className="ml-2"></div>
-        <ZoomController />
+        <div className="mx-1 h-4 w-1 bg-neutral-200 dark:bg-neutral-500"></div>
         <DeleteAllCacheButton />
       </div>
     </>
